760f33ad90abd8e1887eec6e77fe56b68a6a350a
ajay0120 <potugantiajay0120@gmail.com>
2025-04-01 20:00:34 +0530

    added alert if user tries to go to private routes

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -4,7 +4,13 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
-  return token ? children : <Navigate to="/login" />;
+  if(token){
+    return children;
+  }
+  // If no token, redirect to login page. 
+  alert("Please login first");
+  return <Navigate to="/login"/>;
+  //return token ? children : <Navigate to="/login" />;
 };
 
 
