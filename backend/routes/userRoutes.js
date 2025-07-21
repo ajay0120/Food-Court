7bac14667c47b9ab611bd26dc4feee4538aae1d6
ajay0120 <potugantiajay0120@gmail.com>
2025-07-21 23:39:23 +0530

    updated user.js to User.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
-const User = require("../models/user");
+const User = require("../models/User");
 const Order = require("../models/order");
 
 router.get("/profile", protect, (req, res) => {
