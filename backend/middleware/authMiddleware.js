d7bcd252b14ffee9c280915259d23f7f21f9f9d4
ajay0120 <potugantiajay0120@gmail.com>
2025-09-22 19:10:54 +0530

    Refactor ProtectedRoute component; Integrate jwt-decode for improved token handling

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,10 +5,6 @@ const protect = async (req, res, next) => {
   if (!token) return res.status(401).json({ message: "Not authorized, no token" });
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    // const expTime = decoded.exp * 1000;
-    // if (Date.now() > expTime) {
-    //   return res.status(401).json({ message: "Token expired" });
-    // }
     req.user = await User.findById(decoded.id).select("-password");
     if (!req.user) {
       return res.status(401).json({ message: "User not found" });
